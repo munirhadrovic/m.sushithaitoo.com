32e9bd4e6a6c1d17306552344f9e87ac5c5bc385
novicaa <novicamiketic@gmail.com>
2015-03-18 14:44:47 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="869 103rd Avenue North Naples, Fl 34108,US. "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+$telephone="2395927575";// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
